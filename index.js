21ba701cf4fc8f8888389f9ca30d1f233dfac2e5
IrishTnT <contact@irishtnt.com>
2023-02-14 11:10:04 +0000

    Code is now functional.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import { join, dirname } from "node:path";
 import { fileURLToPath } from "node:url";
-import sss from "shamirs-secret-sharing";
+import secrets from "secrets.js-grempe";
 
 const app = express();
 const PORT = 8080;
@@ -17,10 +17,25 @@ app.get("/", (req, res) => {
     res.sendFile(join(__dirname, "/index.html"));
 });
 
-function secretGen(){
-    res.sendFile(join(__dirname, "/secret.html"));
-    const secret = req.query.secret;
-    const shares = sss.split(secret, 5, 3);
-}
+app.get("/generate", (req, res) => {
+    let shares = Number(req.query.shares);
+    let threshold = Number(req.query.threshold);
+    let secret = secrets.str2hex(req.query.secret);
+    
+    let generated = secrets.share(secret, shares, threshold);
+
+    res.send(generated);
+});
+
+app.get("/decode", (req, res) => {
+    let shares = (req.query.shares).split("\r\n");
+
+    let combined = secrets.combine(shares);
+    let recovered = secrets.hex2str(combined);
+
+    console.log(shares);
+
+    res.send(recovered);
+});
 
 app.listen(8080);
--- node_modules/shamirs-secret-sharing/index.js
+++
@@ -1,7 +0,0 @@
-const { combine } = require('./combine')
-const { split } = require('./split')
-
-module.exports = {
-  combine,
-  split
-}
